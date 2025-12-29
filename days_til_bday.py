88e5e84b0bf921b97f488172dd40e10100a11f87
seyon311 <seyonsaithridevrajaguru@gmail.com>
2025-12-29 20:16:00 +0000

    home activities

---
+++ days_til_bday.py
@@ -0,0 +1,25 @@
+import random 
+import datetime
+
+def bdaymessagespy():
+
+  bday_messages = ['Hope you have a very Happy Birthday! 🎈',
+  'Its your special day – get out there and celebrate! 🎉',
+  'You were born and the world got better – everybody wins! 🥳',
+  'Have lots of fun on your special day! 🎂',
+  'Another year of you going around the sun! 🌞'] 
+
+  random_message = random.choice(bday_messages)
+
+today = datetime.date.today()
+next_birthday = datetime.date(2026, 3, 11)
+
+days_away = next_birthday - today
+
+if today == next_birthday:
+  print(bdaymessagespy())
+else:
+  print('My next birthday is ', days_away ,' days away!')
+
+
+
